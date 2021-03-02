e13a569cba2db725be3af0a2573d2e2da78b0942
“First <Kerger06@aol.com>
2021-03-02 09:02:20 -0600

    added route params to user places

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PlaceList from '../components/PlaceList';
+import { useParams } from 'react-router-dom';
 
 const DUMMY_PLACES = [
     {
@@ -45,9 +46,10 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
     
-    
-    return <PlaceList items={DUMMY_PLACES} />
+    return <PlaceList items={ loadedPlaces } />
 };
 
 export default UserPlaces;
